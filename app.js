239a0a4c59a8ec3978fa935e75be4ccd652b2f75
SierraHudson <SierraHudson327@gmail.com>
2020-10-19 23:49:04 -0400

    More Javascript Homework

---
+++ app.js
@@ -0,0 +1,28 @@
+var objOne = {
+    name: "Ashley",
+    age: 32,
+    friends: ["Hunter", "Wayne"]
+};
+
+var objTwo = {
+    name: "Salem",
+    age: 6,
+    friends: ["Lucy", "Dexter"]
+};
+
+var objThree = {
+    name: "Sierra",
+    age: 27,
+    friends: ["Silvia", "Nicole"]
+};
+
+console.log(objOne.name);
+console.log(objTwo.age);
+console.log(objThree.friends[1]);
+
+
+let arr = [5, 10, 15, "Hello", "World"];
+
+for(var i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
